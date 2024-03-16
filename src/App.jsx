927923e53e42d7349095d3b210b633a5b764bc8c
abunaim5 <abunaim140@gmail.com>
2024-03-16 14:12:46 +0600

    load recipe data

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 import Header from './components/Header/Header'
 import Main from './components/Main/Main'
 
 function App() {
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    fetch('recipe.json')
+      .then(res => res.json())
+      .then(data => setRecipes(data))
+  }, [])
+
   return (
     <>
       <Header></Header>
-      <Main></Main>
+      <Main recipes={recipes}></Main>
     </>
   )
 }
